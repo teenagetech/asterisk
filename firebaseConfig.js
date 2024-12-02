3d262e528c60033336bac04a975de026d59c8ec3
Owen Van Vooren <hi@owen.uno>
2024-12-02 16:04:29 -0600

    AutoUpdate

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,13 +1,14 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-analytics.js";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyARDxDlTMVvpm_deaut9qwy2qrtWG9rZBQ",
+  apiKey: "AIzaSyCPd-AcalcZCkYSzWkdVh8bshvj1gTDp_o",
   authDomain: "asterisk-search.firebaseapp.com",
   projectId: "asterisk-search",
   storageBucket: "asterisk-search.firebasestorage.app",
   messagingSenderId: "416571459462",
   appId: "1:416571459462:web:7b75ee6fbea358c13c8e07",
-  measurementId: "G-NQYXW6TM7"
+  measurementId: "G-NQYXW6TM70"
 };
 
 const app = initializeApp(firebaseConfig);
